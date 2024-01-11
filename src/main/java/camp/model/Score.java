dc1c9d7bfbac5558767d921d287b0b3018c04df5
JungJaeHyun <chwogus33@naver.com>
2024-01-11 16:25:32 +0900

    Score 수정기능

--- src/main/java/camp/model/Score.java
+++ src/main/java/camp/model/Score.java
@@ -1,7 +1,16 @@
 package camp.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Score {
     private String scoreId;
+    private String studentId;
+    private String subjectId;
+    private int round;
+    private int score2;
+    private HashMap<Integer, Integer> score;    // round, score
+    private String grade;
 
     public Score(String seq) {
         this.scoreId = seq;
@@ -12,4 +21,20 @@ public class Score {
         return scoreId;
     }
 
+    public String getStudentId() { return studentId; }
+
+    public String getSubjectId() { return subjectId; }
+
+    public Map<Integer, Integer> getScore() { return score; }
+
+    public String getGrade() { return grade; }
+
+    // Setter
+    public void setStudentId(String studentId) { this.studentId = studentId; }
+
+    public void setSubjectId(String subjectId) { this.subjectId = subjectId; }
+
+    public void setScore(int key, int value) { this.score.put(key, value); }
+
+    public void setGrade(String grade) { this.grade = grade; }
 }
